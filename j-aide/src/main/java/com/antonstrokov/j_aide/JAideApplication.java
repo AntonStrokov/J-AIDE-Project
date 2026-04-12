6170717108bf46b08e0a38bfdf7bb570eb7ad288
anton_strokov <89173442264@mail.ru>
2026-04-12 13:36:21 +0500

    first commit

---
+++ j-aide/src/main/java/com/antonstrokov/j_aide/JAideApplication.java
@@ -0,0 +1,13 @@
+package com.antonstrokov.j_aide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JAideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JAideApplication.class, args);
+	}
+
+}
---
+++ j-aide/src/test/java/com/antonstrokov/j_aide/JAideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonstrokov.j_aide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JAideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
