e95bf1d621919740451db2b6cc6288d70a220738
anton_strokov <89173442264@mail.ru>
2026-05-03 13:59:59 +0500

    Improve IntelliJ tool window initial text

--- j-aide/j-aide-intellij-plugin/src/main/java/com/antonstrokov/jaide/plugin/state/JaideResultState.java
+++ j-aide/j-aide-intellij-plugin/src/main/java/com/antonstrokov/jaide/plugin/state/JaideResultState.java
@@ -2,7 +2,14 @@ package com.antonstrokov.jaide.plugin.state;
 
 public class JaideResultState {
 
-	private static String latestSummary = "Select code and run J-Aide: Explain Selected Code";
+	private static String latestSummary = """
+        J-Aide Assistant
+
+        Select code in the editor, right-click, and choose:
+        J-Aide: Explain Selected Code
+
+        The AI explanation will appear here.
+        """;
 
 	public static String getLatestSummary() {
 		return latestSummary;
