0f3843066ccaf36645e65cab7121ef9dc111d5df
anton_strokov <89173442264@mail.ru>
2026-05-24 13:23:13 +0500

    Persist selected explain mode

--- j-aide/j-aide-intellij-plugin/src/main/java/com/antonstrokov/jaide/plugin/state/JaideExplainModeState.java
+++ j-aide/j-aide-intellij-plugin/src/main/java/com/antonstrokov/jaide/plugin/state/JaideExplainModeState.java
@@ -1,24 +1,33 @@
 package com.antonstrokov.jaide.plugin.state;
 
 import com.antonstrokov.jaide.plugin.model.JaideExplainMode;
+import com.intellij.ide.util.PropertiesComponent;
 
 public final class JaideExplainModeState {
-
-	private static JaideExplainMode currentMode = JaideExplainMode.SMART;
+	private static final String EXPLAIN_MODE_KEY = "j-aide.explain.mode";
 
 	private JaideExplainModeState() {
 	}
 
 	public static JaideExplainMode getCurrentMode() {
-		return currentMode;
+		String modeName = PropertiesComponent.getInstance().getValue(EXPLAIN_MODE_KEY);
+
+		if (modeName == null || modeName.isBlank()) {
+			return JaideExplainMode.SMART;
+		}
+
+		try {
+			return JaideExplainMode.valueOf(modeName);
+		} catch (IllegalArgumentException ex) {
+			return JaideExplainMode.SMART;
+		}
 	}
 
 	public static void setCurrentMode(JaideExplainMode mode) {
-		if (mode == null) {
-			currentMode = JaideExplainMode.SMART;
-			return;
-		}
+		JaideExplainMode modeToStore = mode == null
+				? JaideExplainMode.SMART
+				: mode;
 
-		currentMode = mode;
+		PropertiesComponent.getInstance().setValue(EXPLAIN_MODE_KEY, modeToStore.name());
 	}
 }
