9a8a8f4a0c8244f5d582563ffb5e82e62ceda23c
anton_strokov <89173442264@mail.ru>
2026-06-23 15:51:27 +0500

    Inject AI properties into provider health

--- j-aide/j-aide-core/src/main/java/com/antonstrokov/j_aide/core/service/AiProviderHealthService.java
+++ j-aide/j-aide-core/src/main/java/com/antonstrokov/j_aide/core/service/AiProviderHealthService.java
@@ -3,14 +3,20 @@ package com.antonstrokov.j_aide.core.service;
 import com.antonstrokov.j_aide.core.dto.health.AiProviderHealthResult;
 import com.antonstrokov.j_aide.core.dto.health.AiProviderHealthStatus;
 import com.antonstrokov.j_aide.core.integration.ollama.OllamaClient;
+import com.antonstrokov.j_aide.core.config.AiProperties;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AiProviderHealthService {
 	private final OllamaClient ollamaClient;
+	private final AiProperties aiProperties;
 
-	public AiProviderHealthService(OllamaClient ollamaClient) {
+	public AiProviderHealthService(
+			OllamaClient ollamaClient,
+			AiProperties aiProperties
+	) {
 		this.ollamaClient = ollamaClient;
+		this.aiProperties = aiProperties;
 	}
 
 	public AiProviderHealthResult getHealthInfo() {
