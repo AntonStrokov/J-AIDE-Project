4c8693efa9bf461f3f776c0c07d8f3a1ce10f6c3
anton_strokov <89173442264@mail.ru>
2026-06-23 14:57:39 +0500

    Inject AI properties into provider health service

--- j-aide/j-aide-core/src/main/java/com/antonstrokov/j_aide/core/service/AiProviderHealthService.java
+++ j-aide/j-aide-core/src/main/java/com/antonstrokov/j_aide/core/service/AiProviderHealthService.java
@@ -1,11 +1,17 @@
 package com.antonstrokov.j_aide.core.service;
 
+import com.antonstrokov.j_aide.core.config.AiProperties;
 import com.antonstrokov.j_aide.core.dto.health.AiProviderHealthResult;
 import com.antonstrokov.j_aide.core.dto.health.AiProviderHealthStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AiProviderHealthService {
+	private final AiProperties aiProperties;
+
+	public AiProviderHealthService(AiProperties aiProperties) {
+		this.aiProperties = aiProperties;
+	}
 
 	public AiProviderHealthResult getHealthInfo() {
 		return new AiProviderHealthResult(
