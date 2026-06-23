dfd2a43a55d7cc444cd1134870e39e1d99b8f0f6
anton_strokov <89173442264@mail.ru>
2026-06-23 15:49:08 +0500

    Add Ollama models client method

--- j-aide/j-aide-core/src/main/java/com/antonstrokov/j_aide/core/integration/ollama/OllamaClient.java
+++ j-aide/j-aide-core/src/main/java/com/antonstrokov/j_aide/core/integration/ollama/OllamaClient.java
@@ -1,9 +1,10 @@
 package com.antonstrokov.j_aide.core.integration.ollama;
 
+import com.antonstrokov.j_aide.core.config.AiProperties;
+import com.antonstrokov.j_aide.core.integration.ollama.dto.OllamaTagsResponse;
+import com.antonstrokov.j_aide.core.integration.ollama.dto.OllamaVersionResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
-import com.antonstrokov.j_aide.core.config.AiProperties;
-import com.antonstrokov.j_aide.core.integration.ollama.dto.OllamaVersionResponse;
 
 @Component
 public class OllamaClient {
@@ -23,5 +24,16 @@ public class OllamaClient {
 
 	private String buildVersionUrl() {
 		return aiProperties.ollama().baseUrl() + "/api/version";
+	}
+
+	public OllamaTagsResponse getModels() {
+		return restClient.get()
+				.uri(buildTagsUrl())
+				.retrieve()
+				.body(OllamaTagsResponse.class);
+	}
+
+	private String buildTagsUrl() {
+		return aiProperties.ollama().baseUrl() + "/api/tags";
 	}
 }
