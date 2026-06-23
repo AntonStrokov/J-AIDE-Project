15d9d2a73722533cef60af07599ea31fb8541cad
anton_strokov <89173442264@mail.ru>
2026-06-23 15:34:30 +0500

    Inject Ollama client into health service

--- j-aide/j-aide-core/src/main/java/com/antonstrokov/j_aide/core/service/AiProviderHealthService.java
+++ j-aide/j-aide-core/src/main/java/com/antonstrokov/j_aide/core/service/AiProviderHealthService.java
@@ -2,10 +2,16 @@ package com.antonstrokov.j_aide.core.service;
 
 import com.antonstrokov.j_aide.core.dto.health.AiProviderHealthResult;
 import com.antonstrokov.j_aide.core.dto.health.AiProviderHealthStatus;
+import com.antonstrokov.j_aide.core.integration.ollama.OllamaClient;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AiProviderHealthService {
+	private final OllamaClient ollamaClient;
+
+	public AiProviderHealthService(OllamaClient ollamaClient) {
+		this.ollamaClient = ollamaClient;
+	}
 
 	public AiProviderHealthResult getHealthInfo() {
 		return new AiProviderHealthResult(
