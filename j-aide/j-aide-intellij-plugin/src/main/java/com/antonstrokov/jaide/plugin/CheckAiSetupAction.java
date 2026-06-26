6704b423eaecd91e7aad478f453e01aab98c2a2a
anton_strokov <89173442264@mail.ru>
2026-06-26 20:57:43 +0500

    Delegate AI setup action to service

--- j-aide/j-aide-intellij-plugin/src/main/java/com/antonstrokov/jaide/plugin/CheckAiSetupAction.java
+++ j-aide/j-aide-intellij-plugin/src/main/java/com/antonstrokov/jaide/plugin/CheckAiSetupAction.java
@@ -1,99 +1,16 @@
 package com.antonstrokov.jaide.plugin;
 
-import com.antonstrokov.jaide.plugin.client.JaideBackendClient;
-import com.antonstrokov.jaide.plugin.config.JaideConstants;
-import com.antonstrokov.jaide.plugin.dto.health.JaideHealthResponse;
-import com.antonstrokov.jaide.plugin.dto.health.JaideHealthStatus;
-import com.antonstrokov.jaide.plugin.error.JaideErrorMessageBuilder;
-import com.antonstrokov.jaide.plugin.notification.JaideNotificationService;
+import com.antonstrokov.jaide.plugin.service.JaideAiSetupCheckService;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.Task;
-import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
 public class CheckAiSetupAction extends AnAction {
-	private static final Logger log =
-			Logger.getInstance(CheckAiSetupAction.class);
-
-	private final JaideBackendClient backendClient =
-			new JaideBackendClient();
-
-	private final JaideErrorMessageBuilder errorMessageBuilder =
-			new JaideErrorMessageBuilder();
-
-	private final JaideNotificationService notificationService =
-			new JaideNotificationService();
+	private final JaideAiSetupCheckService aiSetupCheckService =
+			new JaideAiSetupCheckService();
 
 	@Override
 	public void actionPerformed(@NotNull AnActionEvent event) {
-		Project project = event.getProject();
-
-		new Task.Backgroundable(
-				project,
-				JaideConstants.CHECK_AI_SETUP_TASK_TITLE,
-				false
-		) {
-			@Override
-			public void run(@NotNull ProgressIndicator indicator) {
-				try {
-					JaideHealthResponse response =
-							backendClient.checkAiHealth();
-
-					log.info(
-							"AI setup check completed, backendStatus="
-									+ response.backendStatus()
-									+ ", providerStatus="
-									+ response.providerStatus()
-									+ ", modelStatus="
-									+ response.modelStatus()
-									+ ", responseTimeMs="
-									+ response.responseTimeMs()
-					);
-
-					showHealthResult(project, response);
-				} catch (Exception exception) {
-					log.warn(
-							"AI setup check failed: "
-									+ exception.getMessage(),
-							exception
-					);
-
-					notificationService.showError(
-							project,
-							errorMessageBuilder.build(exception)
-					);
-				}
-			}
-		}.queue();
-	}
-
-	private void showHealthResult(
-			Project project,
-			JaideHealthResponse response
-	) {
-		if (hasStatus(response, JaideHealthStatus.FAILED)) {
-			notificationService.showError(project, response.message());
-			return;
-		}
-
-		if (hasStatus(response, JaideHealthStatus.DEGRADED)
-				|| hasStatus(response, JaideHealthStatus.UNKNOWN)) {
-			notificationService.showWarning(project, response.message());
-			return;
-		}
-
-		notificationService.showInfo(project, response.message());
-	}
-
-	private boolean hasStatus(
-			JaideHealthResponse response,
-			JaideHealthStatus status
-	) {
-		return response.backendStatus() == status
-				|| response.providerStatus() == status
-				|| response.modelStatus() == status;
+		aiSetupCheckService.check(event.getProject());
 	}
 }
